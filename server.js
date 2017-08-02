972e607e9a5289759f45157c5436a86184dc001c
Raj K <raj.kandati@gmail.com>
2017-08-02 09:20:07 -0400

    Added a projects file

--- server.js
+++ server.js
@@ -44,6 +44,13 @@ app.get('/about', (req, res) => {
   });
 });
 
+app.get('/projects', (req, res) => {
+  res.render('projects.hbs', {
+    pageTitle: 'Projects Page',
+    welcomeMessage: 'Portfolio Page'
+  });
+});
+
 // /bad - send back json with errorMessage
 app.get('/bad', (req, res) => {
   res.send({
